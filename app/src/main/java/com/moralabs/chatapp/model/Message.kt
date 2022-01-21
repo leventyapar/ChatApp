d92380380ef5c1840a92ea551ca426fc2b74a75b
Levent <leventyapar@gmail.com>
2022-01-21 14:03:10 +0300

    Initial commit

---
+++ app/src/main/java/com/moralabs/chatapp/model/Message.kt
@@ -0,0 +1,7 @@
+package com.moralabs.chatapp.model
+
+data class Message(
+    val chatId : String =  "",
+    val message : String = "",
+    val sentBy : String = ""
+)
---
+++ app/src/main/java/com/moralabs/chatapp/model/User.kt
@@ -0,0 +1,10 @@
+package com.moralabs.chatapp.model
+
+data class User(
+    val userId : String = "",
+    val userName : String = "",
+    val userNickname: String = "",
+    val userSurname : String = "",
+    val userEmail : String = "",
+    val userPicture : String = ""
+)
